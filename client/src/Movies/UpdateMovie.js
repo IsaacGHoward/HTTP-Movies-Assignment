8450f824ef84a9c986e54bd1ed118a63d69b912c
IsaacGHoward <howardisaac100@gmail.com>
2021-01-19 16:48:44 -0800

    npm install, set up UpdateMovie

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+
+function UpdateMovie() {
+
+  return (
+    <div>
+      Update Movie
+    </div>
+  );
+}
+
+export default UpdateMovie;
